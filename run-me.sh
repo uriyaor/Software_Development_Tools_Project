a6f01b90f244a8ec541b1047a7805d5b5972fd00
uriyaor <120022946+uriyaor@users.noreply.github.com>
2025-02-14 11:29:52 +0200

    Create run-me.sh

---
+++ run-me.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Download the repository from GitHub
+git clone https://github.com/uriyaor/drupal-docker-backup.git
+
+# Change to the repository directory
+cd drupal-docker-backup
+
+# Make the script executable
+chmod +x setup.sh
+
+# Run the script
+./setup.sh
